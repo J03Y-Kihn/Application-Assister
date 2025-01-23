63739397a059d22565ef07d063681fe03683a3ef
Joseph Kihn <71400763+J03Y-Kihn@users.noreply.github.com>
2025-01-23 16:08:10 -0500

    Update Rows
    Can now Update existing rows overriding the data

--- applicationAdder.py
+++ applicationAdder.py
@@ -6,8 +6,9 @@ TITLE_ROW = 1   #change if your titles are not in this row
 STARTING_COLUMN = 2 #change if your format is different
 STARTING_ROW = 2    #change if your format is different
 
-#add a row to the file at the highest unused location
+#adds a new row to the file, prompting the user for data for each column
 def addRow(file):
+
 
     #load excel file
     workbook = load_workbook(file)
@@ -41,40 +42,116 @@ def addRow(file):
     workbook.save(file)
     workbook.close()
 
-#not implemented will update the input row
+#updates the desired row in the file, prompting user for new data
 def updateRow(file):
-    """
+    
     #load excel file
     workbook = load_workbook(file)
     #open workbook
     sheet = workbook.active
 
-    currColumn = STARTING_COLUMN
-    updateRow = sheet.max_row+1
-    while(currColumn<sheet.max_column+1):
-        print("Enter the", sheet.cell(row= TITLE_ROW, column= currColumn).value)
-        currInput = input()
-        currCell = sheet.cell(updateRow, currColumn)
-        currCell.value = currInput
+    #prompt for position or company name
+    print("\nNote: Enter specific information to find fewer applications:")
+    rows = validCompPos(file)
 
-        currColumn+=1
+    if(rows):
+        print("")
+        for i in range(len(rows)):
+            currColumn = STARTING_COLUMN
+            while(currColumn<sheet.max_column+1):
+                print(currColumn-1, sheet.cell(row= TITLE_ROW, column= currColumn).value + ":", sheet.cell(row= rows[i], column = currColumn).value)
+                currColumn+=1
+            print("")
+
+            print("Update this information? \nType \"Yes\" to Update \nType \"Back\" to return to previous Application \nType \"Menu\" to return to main menu \nAny other input or noninput will move you to the next application if there is one")
+            inputs = input()
+
+            if(inputs.lower() == "menu"):
+                workbook.close()
+                return
+            elif(inputs.lower() == "back"):
+                i -= 2
+            elif(inputs.lower() == "yes" or inputs.lower() == "y"):
+                updated = False
+                while(not updated):
+                    print("Which row to update?")
+                    inputs = input()
+
+                    upCol = getCol(file, inputs)
+                    if(inputs.isnumeric() and int(inputs)+1 <= sheet.max_column+1 and int(inputs)+1 >= STARTING_COLUMN):
+                        upCol = int(inputs)+1
+                        print("What should it be updated to?")
+                        inputs = input()
+
+                        sheet.cell(rows[i], upCol).value = inputs
+                        updated = True
+                        workbook.save(file)
+                    elif(upCol):
+                        print("What should it be updated to?")
+                        inputs = input()
+
+                        sheet.cell(rows[i], upCol).value = inputs
+                        updated = True
+                        workbook.save(file)
+
 
     #save file
     workbook.save(file)
     workbook.close()
-    """
-
-#outputs all information in a given row
-def viewRow(file):
+    
+#gets the current row of the input value to check
+def getCol(file, check):
     #load excel file
     workbook = load_workbook(file)
     #open workbook
     sheet = workbook.active
 
-    #Determine if the input Company or Position is Valid
+    currCol = STARTING_COLUMN
+    currTitle = sheet.cell(TITLE_ROW, currCol)
+    while(currTitle.value):
+        if(currTitle.value.lower() == check.lower()):
+            workbook.close()
+            return currCol
+
+        currCol+=1
+        currTitle = sheet.cell(TITLE_ROW, currCol)
+        
+        
+    return False
+
+def viewRow(file):
+    
+    ###Add a flag to all for partial matching rather than full matching? ###
+
+    #load excel file
+    workbook = load_workbook(file)
+    #open workbook
+    sheet = workbook.active
+
+    posRow = validCompPos(file)
+        
+    
+    for values in posRow:
+        currColumn = STARTING_COLUMN
+        while(currColumn<sheet.max_column+1):
+            print(sheet.cell(row= TITLE_ROW, column= currColumn).value + ":", sheet.cell(row= values, column = currColumn).value)
+            currColumn+=1
+        print("")
+
+    print("Number of applications found:", len(posRow))
+    #save file
+    workbook.save(file)
+    workbook.close()
+    
+#Determines if the input company or position name is valid or exits back to main menu otherwise
+#If it is valid, gets the row or rows that the company or position name resides in the excel file
+def validCompPos(file):
     inputs = ""
     print("Which Company or Position would you like to view?")
     inputs = input()
+
+    if(inputs.lower() == "back"):
+        return
     posRow = getRow(file, inputs)
     while(not posRow):
         print("Row is not in file! Please double check your spelling and try again or type 'back' to return to menu.")
@@ -85,21 +162,10 @@ def viewRow(file):
             return
         else:
             posRow = getRow(file, inputs)
-        
-    currColumn = STARTING_COLUMN
-    for values in posRow:
-        while(currColumn<sheet.max_column+1):
-            print(sheet.cell(row= TITLE_ROW, column= currColumn).value + ":", sheet.cell(row= values, column = currColumn).value)
-            currColumn+=1
-        print("")
-        currColumn = STARTING_COLUMN
 
-    print("Number of applications found:", len(posRow))
-    #save file
-    workbook.save(file)
-    workbook.close()
-
-#returns the row number if a row in the file has the same name as position in select columns
+    return posRow
+    
+#returns the row or rows of the desired position
 def getRow(file, position):
 
     #load excel file
@@ -130,7 +196,7 @@ def getRow(file, position):
     else:
         return False
 
-#outputs the number of applications(rows) in the file
+#Prints out the number of rows (applications completed) in the excel file
 def numAppsComp(file):
     #load excel file
     workbook = load_workbook(file, data_only=True)
@@ -140,7 +206,7 @@ def numAppsComp(file):
     print(sheet.max_row -1)
     workbook.close()
 
-
+#main menu that runs
 def main():
     default = False
     print("Welcome to the Application Adder!! \nIf adding to a file that is not", DEFAULT_FILE, "please type it now, otherwise hit enter.")
@@ -155,12 +221,12 @@ def main():
     inputs = ""
     while(inputs.upper() != "0"):
         print("NOTE!! If file is open, no additions can be added only viewing!")
-        print("1: Add new row to spreadsheet? \n2: Update existing row? (Not Implemented) \n3: View Row Data? \n4: View Number of Applications \n0: Exit Program")
+        print("1: Add new row to spreadsheet? \n2: Update existing row? \n3: View Row Data? \n4: View Number of Applications \n0: Exit Program")
         inputs = input()
         if(inputs == "1"):
             addRow(filenames)
         elif(inputs == "2"):
-            #updateRow(filenames)
+            updateRow(filenames)
             print("Update Completed")
         elif(inputs == "3"):
             viewRow(filenames)
@@ -172,4 +238,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    print("Good Night!")
